2e6dcb8fe956bafe5c256f3f8874d09a25b9c38e
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-01 17:56:48 -0300

    create "head.jsx" file

---
+++ app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head()  {
+    return (
+        <>
+            <title>Forecast Weather</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Web Forecast Weather"/>
+            <link rel="icon" href="/icon.png"/>
+        </>
+    )
+    
+}
