74893a4619cb7f3115147cd977fe138194d526fd
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-04 15:46:37 -0300

    import and use "useRouter & useGlobalContext" and create "changeToForecastRoute" function

--- src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/index.jsx
+++ src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/index.jsx
@@ -1,17 +1,34 @@
+'use client'
+
 import { GoogleAutocompleteStyledComponent } from "./style"
 import { usePlacesWidget } from "react-google-autocomplete";
+import { useRouter } from "next/navigation";
+import { useGlobalContext } from "@/src/components/ContextProviders";
 
-export default function GoogleAutocomplete( { setSearchPlace } ) {
+export default function GoogleAutocomplete() {
     
     const apiKeyGoogleAutocomplete = process.env.NEXT_PUBLIC_GOOGLE_AUTOCOMPLETE_API_KEY
 
+    const { forecast, setForecast } = useGlobalContext()
+    
+
     function getPlaceLatLngAndAdress ( place ) {
         if( place != null ) {
-            setSearchPlace( {
+            const placeLatLngAndAdress = {
                 placeName: place.formatted_address,
                 lat: place.geometry.location.lat(),
                 lng: place.geometry.location.lng()
-            } )
+            } 
+            setForecast( placeLatLngAndAdress )
+            changeToForecastRoute( placeLatLngAndAdress )
+        } 
+    }
+
+    const router = useRouter()
+    function changeToForecastRoute( placeLatLngAndAdress ) {
+        if( placeLatLngAndAdress != null) {
+            const pathName = `/${ placeLatLngAndAdress.lat }/${ placeLatLngAndAdress.lng }`
+            router.push(pathName, placeLatLngAndAdress )            
         }
     }
 
@@ -23,6 +40,7 @@ export default function GoogleAutocomplete( { setSearchPlace } ) {
 
     return (
         <GoogleAutocompleteStyledComponent>
+            <span>Escolha uma cidade !</span>
             <input ref={ ref } type="text" />
         </GoogleAutocompleteStyledComponent>
     )
