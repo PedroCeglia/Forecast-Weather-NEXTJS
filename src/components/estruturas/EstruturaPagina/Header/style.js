c46ac6696d17d9c0b82e750581d39a7fdb7a800a
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-04 11:13:26 -0300

    create "InitialPagePadding" and set "HeaderContainerStyle", styled-components

--- src/components/estruturas/EstruturaPagina/Header/style.js
+++ src/components/estruturas/EstruturaPagina/Header/style.js
@@ -9,5 +9,20 @@ export const HeaderContainerStyle = styled.header`
 
     width: 100%;
     height: 100px;
-    background-color: red;
+    background-color: rgb(25, 115, 199);
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    a {
+        font-size: 2em;
+        font-weight: bold;
+        text-decoration: none;
+        color: rgba( 0, 0, 0, 1 );
+    }
+`
+
+export const InitialPagePadding = styled.div`
+    padding-top: 100px;
 `
