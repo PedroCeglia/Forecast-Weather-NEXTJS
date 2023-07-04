e032010859db166a8d815f039533fdb08029425a
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-04 11:08:53 -0300

    import and use "ContextProviders" functions

--- src/components/estruturas/EstruturaPagina/index.jsx
+++ src/components/estruturas/EstruturaPagina/index.jsx
@@ -1,15 +1,18 @@
 import { EstruturaPaginaStyle, ConteudoPaginaContainerStyle } from "./style"
 import Header from "./Header"
 import Footer from "./Footer"
+import ContextProviders from "../../ContextProviders"
 
 export default function EstruturaPagina( { children } ) {
     return (
         <EstruturaPaginaStyle>
-            <Header/>
-            <ConteudoPaginaContainerStyle>
-                { children }
-            </ConteudoPaginaContainerStyle>
-            <Footer/>
+            <ContextProviders>
+                <Header/>
+                <ConteudoPaginaContainerStyle>
+                    { children }
+                </ConteudoPaginaContainerStyle>
+                <Footer/>                
+            </ContextProviders>
         </EstruturaPaginaStyle>
     )
 }
