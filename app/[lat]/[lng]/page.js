674324ea61140e0aa39281d70a35721280af0925
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-04 11:05:48 -0300

    Create the "ForecastPage" component

---
+++ app/[lat]/[lng]/page.js
@@ -0,0 +1,9 @@
+import { IntroMainSection } from "@/src/components/pages/Main"
+
+export default function ForecastPage() {
+    return (
+        <>
+            <IntroMainSection/>
+        </>
+    )
+}
