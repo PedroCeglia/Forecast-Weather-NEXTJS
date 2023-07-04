44a980ee1c20b4e174da1b0dee167dd50c2b5769
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-04 15:47:19 -0300

    add a "span" style in "GoogleAutocompleteStyledComponent"

--- src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/style.js
+++ src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/style.js
@@ -2,27 +2,33 @@
 
 import styled from "styled-components"
 
-export const GoogleAutocompleteStyledComponent= styled.div`
+export const GoogleAutocompleteStyledComponent = styled.div`
     
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
 
-    padding: 0 10px;
     max-width: 450px;
     width: 100%;
     min-width: 250px;
 
-    border-radius: 10px;
-    background-color: #efefef8c;
-    border: 1px solid #efefef;
 
     input{
         width: 100%;
-        padding: 10px 20px;
+        padding: 10px 30px;
+        border-radius: 10px;
 
         border: none;
         outline: none;
         background-color: transparent;
+
+        background-color: #efefef8c;
+        border: 1px solid #efefef;
+    }
+    span {
+        margin-left: 5px;
+        margin-bottom: 5px;
+        font-size: 1em;
     }
 `
