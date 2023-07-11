2f04e1f14033435792320550099ee2c5c8802ca4
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-07-11 01:26:01 -0300

    create "light" theme

---
+++ src/style/ThemeProvider/light.js
@@ -0,0 +1,9 @@
+export default {
+    type: "light",
+    mainColor: "rgb(169, 214, 229)",
+    headerColor: "rgb(137, 194, 217)",
+    articleForecastColor: "rgb(97, 165, 194)",
+    tableForecastColor: "rgb(70, 143, 175)",
+    scrollColor: "rgb(44, 125, 160)",
+    textColor: "rgb(1, 42, 74)"
+}
