0919016fa795a5831f9739b99c6bd3b785552d3d
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-08-09 11:05:34 -0300

    Create "ForecastPageStyle &

---
+++ src/components/pages/Forecast/style.js
@@ -0,0 +1,21 @@
+'use client'
+
+import styled from "styled-components"
+
+export const ForecastPageStyle = styled.div`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+` 
+export const CurrentAndDayForecastStyle = styled.div`
+    width: 60vw;
+    height:  80vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+` 
