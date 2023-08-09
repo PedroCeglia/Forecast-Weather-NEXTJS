b3657f128696357c466cdcb0cd6c1b2a2a9c7f1b
PedroCeglia <dev.ceglia.pedro@gmail.com>
2023-08-09 10:59:37 -0300

    delete "changeToForecastRoute" and "router", Set "setForecast" to "onChangeForecastPlace"

--- src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/index.jsx
+++ src/components/estruturas/EstruturaPagina/Header/GoogleAutocomplete/index.jsx
@@ -2,15 +2,13 @@
 
 import { GoogleAutocompleteStyledComponent } from "./style"
 import { usePlacesWidget } from "react-google-autocomplete";
-import { useRouter } from "next/navigation";
 import { useGlobalContext } from "@/src/components/ContextProviders";
 import { useThemeProvider } from "@/src/style/ThemeProvider";
 
 export default function GoogleAutocomplete() {
     
-    const { setForecast } = useGlobalContext()
+    const { onChangeForecastPlace } = useGlobalContext()
     const { theme } = useThemeProvider()
-    const router = useRouter()
 
     const { ref } = usePlacesWidget( {
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_AUTOCOMPLETE_API_KEY,
@@ -22,24 +20,17 @@ export default function GoogleAutocomplete() {
             const placeLatLngAndAdress = {
                 placeName: place.formatted_address,
                 lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng()
+                lon: place.geometry.location.lng()
             } 
-            setForecast( placeLatLngAndAdress )
-            changeToForecastRoute( placeLatLngAndAdress )
+            onChangeForecastPlace( placeLatLngAndAdress )
         } 
     }
 
-    function changeToForecastRoute( placeLatLngAndAdress ) {
-        if( placeLatLngAndAdress != null) {
-            const pathName = `/${ placeLatLngAndAdress.lat }/${ placeLatLngAndAdress.lng }`
-            router.push( pathName, placeLatLngAndAdress )            
-        }
-    }
 
     return (
-        <GoogleAutocompleteStyledComponent theme={ theme }>
+        <GoogleAutocompleteStyledComponent theme={ theme } >
             <label>Escolha uma cidade !</label>
-            <input ref={ ref } placeholder="digite algo" type="text" />
+            <input ref={ ref } placeholder="digite algo" type="text"/>
         </GoogleAutocompleteStyledComponent>
     )
 }
